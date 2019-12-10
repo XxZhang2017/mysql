ea75b5035e3882e773dc49622e9fdc04584a1819
XxZhang2017 <mynewvegaan@gmail.com>
2019-12-09 23:32:55 -0800

    add

--- ex02/ex02.sql
+++ ex02/ex02.sql
@@ -1,4 +1,4 @@
-insert into `ft_table`(login, `group`, createion_date) values(loki, staff, 2013-05-01);
+insert into `ft_table`(`login`, `group`, `createion_date`) values(`loki`, `staff`, `2013-05-01`);
 insert into `ft_table`(`login`, `group`, `creation_date`) values('scadoux', 'student', '2014-01-01'),
 	('chap', 'staff', '2011-04-27'),
 	('bambou', 'staff', '2014-03-01'),
